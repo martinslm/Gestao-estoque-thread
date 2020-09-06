e7d4bb99d000699c1c1287902c8d5c3e3398318c
Larissa Martins Mauricio <38192027+martinslm@users.noreply.github.com>
2020-09-06 17:32:15 -0300

    Criação da Solution

---
+++ PPD.GestaoEstoque.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PPD.GestaoEstoque.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
