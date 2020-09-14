638c22b38e7091b79aa15305f09733084e109b65
Larissa Martins Mauricio <larissa.jucre@gmail.com>
2020-09-14 19:41:39 -0300

    Inclusão de novas vendas para fechar a quantidade minima de 50 vendas

--- PPD.GestaoEstoque.ConsoleApp/Database/InMemoryDbContext.cs
+++ PPD.GestaoEstoque.ConsoleApp/Database/InMemoryDbContext.cs
@@ -24,7 +24,61 @@ namespace PPD.GestaoEstoque.ConsoleApp.Database
                 new Venda(2, ObterProdutosVenda2(), 2),
                 new Venda(3, ObterProdutosVenda3(), 3),
                 new Venda(4, ObterProdutosVenda4(), 4),
-                new Venda(5, ObterProdutosVenda5(), 5)
+                new Venda(5, ObterProdutosVenda5(), 5),
+
+                new Venda(6, ObterProdutosVenda1(), 6),
+                new Venda(7, ObterProdutosVenda2(), 7),
+                new Venda(8, ObterProdutosVenda3(), 8),
+                new Venda(9, ObterProdutosVenda4(), 9),
+                new Venda(10, ObterProdutosVenda5(), 10),
+
+                new Venda(11, ObterProdutosVenda1(), 10),
+                new Venda(12, ObterProdutosVenda2(), 9),
+                new Venda(13, ObterProdutosVenda3(),8),
+                new Venda(14, ObterProdutosVenda4(), 7),
+                new Venda(15, ObterProdutosVenda5(), 6),
+
+                 new Venda(16, ObterProdutosVenda1(), 5),
+                new Venda(17, ObterProdutosVenda2(), 4),
+                new Venda(18, ObterProdutosVenda3(), 3),
+                new Venda(19, ObterProdutosVenda4(), 2),
+                new Venda(20, ObterProdutosVenda5(), 1),
+
+                new Venda(21, ObterProdutosVenda1(), 2),
+                new Venda(22, ObterProdutosVenda2(), 1),
+                new Venda(23, ObterProdutosVenda3(), 4),
+                new Venda(24, ObterProdutosVenda4(), 3),
+                new Venda(25, ObterProdutosVenda5(), 6),
+
+                 new Venda(26, ObterProdutosVenda1(), 5),
+                new Venda(27, ObterProdutosVenda2(), 7),
+                new Venda(28, ObterProdutosVenda3(), 8),
+                new Venda(29, ObterProdutosVenda4(), 10),
+                new Venda(30, ObterProdutosVenda5(), 9),
+
+                new Venda(31, ObterProdutosVenda1(), 1),
+                new Venda(32, ObterProdutosVenda2(), 2),
+                new Venda(33, ObterProdutosVenda3(), 3),
+                new Venda(34, ObterProdutosVenda4(), 4),
+                new Venda(35, ObterProdutosVenda5(), 5),
+
+                 new Venda(36, ObterProdutosVenda1(), 6),
+                new Venda(37, ObterProdutosVenda2(), 7),
+                new Venda(38, ObterProdutosVenda3(), 8),
+                new Venda(39, ObterProdutosVenda4(), 9),
+                new Venda(40, ObterProdutosVenda5(), 10),
+
+                new Venda(41, ObterProdutosVenda1(), 10),
+                new Venda(42, ObterProdutosVenda2(), 9),
+                new Venda(43, ObterProdutosVenda3(), 8),
+                new Venda(44, ObterProdutosVenda4(), 7),
+                new Venda(45, ObterProdutosVenda5(), 6),
+
+                new Venda(46, ObterProdutosVenda1(), 5),
+                new Venda(47, ObterProdutosVenda2(), 4),
+                new Venda(48, ObterProdutosVenda3(), 3),
+                new Venda(49, ObterProdutosVenda4(), 2),
+                new Venda(50, ObterProdutosVenda5(), 1)
             };
         }
         private List<Filial> ObterListagemFiliais()
@@ -47,16 +101,16 @@ namespace PPD.GestaoEstoque.ConsoleApp.Database
         {
             return new List<Produto>()
             {
-                new Produto(1, 3, 10),
-                new Produto(2, 10, 15),
-                new Produto(3, 200, 5),
-                new Produto(4, 3, 2),
-                new Produto(5, 8, 1.5m),
-                new Produto(6, 50, 2),
-                new Produto(7, 70, 5),
-                new Produto(8, 90, 8),
-                new Produto(9, 150, 11),
-                new Produto(10, 50, 15)
+                new Produto(1, 30, 10),
+                new Produto(2, 100, 15),
+                new Produto(3, 2000, 5),
+                new Produto(4, 30, 2),
+                new Produto(5, 80, 1.5m),
+                new Produto(6, 500, 2),
+                new Produto(7, 700, 5),
+                new Produto(8, 900, 8),
+                new Produto(9, 1500, 11),
+                new Produto(10, 500, 15)
             };
         }
 
@@ -64,16 +118,16 @@ namespace PPD.GestaoEstoque.ConsoleApp.Database
         {
             return new List<Produto>()
             {
-                new Produto(1, 3, 10),
-                new Produto(2, 10, 15),
+                new Produto(1, 30, 10),
+                new Produto(2, 100, 15),
                 new Produto(3, 200, 5),
-                new Produto(4, 3, 2),
-                new Produto(5, 8, 1.5m),
-                new Produto(6, 50, 2),
-                new Produto(7, 70, 5),
-                new Produto(8, 90, 8),
+                new Produto(4, 30, 2),
+                new Produto(5, 80, 1.5m),
+                new Produto(6, 500, 2),
+                new Produto(7, 700, 5),
+                new Produto(8, 900, 8),
                 new Produto(9, 150, 11),
-                new Produto(10, 50, 15)
+                new Produto(10, 500, 15)
             };
         }
 
@@ -81,16 +135,16 @@ namespace PPD.GestaoEstoque.ConsoleApp.Database
         {
             return new List<Produto>()
             {
-                new Produto(1, 3, 10),
-                new Produto(2, 10, 15),
+                new Produto(1, 30, 10),
+                new Produto(2, 100, 15),
                 new Produto(3, 200, 5),
-                new Produto(4, 3, 2),
-                new Produto(5, 8, 1.5m),
-                new Produto(6, 50, 2),
-                new Produto(7, 70, 5),
-                new Produto(8, 90, 8),
+                new Produto(4, 30, 2),
+                new Produto(5, 80, 1.5m),
+                new Produto(6, 500, 2),
+                new Produto(7, 700, 5),
+                new Produto(8, 900, 8),
                 new Produto(9, 150, 11),
-                new Produto(10, 50, 15)
+                new Produto(10, 500, 15)
             };
         }
 
